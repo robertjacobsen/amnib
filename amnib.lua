937ebb0b24f2bb7c81d7517f95a77455a77b537a
Robert Jacobsen <robert@robertjacobsen.net>
2011-09-05 19:39:08 +0200

    Hopefully made the addon more generic with better code quality.

--- amnib.lua
+++ amnib.lua
@@ -1,10 +1,20 @@
 amnibdb = {}
-local acceptInvites = true
-local currentInstance = nil
+local inviteKeywords = {
+	["inv"] = true,
+	["invite"] = true,
+	["inviter"] = true,
+	["'invite' without the quotes"] = true
+}
+local defaultText = "Invites for %s have started. For an invite, please whisper me 'invite' without the quotes.";
 
-local function Print(pre, green, text)
-	if green == "" then green = "amnib" end
-	ChatFrame1:AddMessage(string.format("%s|cff33ff99%s|r: %s", pre, green, text))
+local function GreenPrint(green, text)
+	if not text or not green then return end
+	ChatFrame1:AddMessage(string.format("|cff33ff99%s|r: %s", green, text))
+end
+
+local function Print(text) 
+	if not text then return end
+	GreenPrint("amnib", text)
 end
 
 -- [[ Initialization of the SavedVariable ]]
@@ -15,83 +25,91 @@ addon:SetScript("OnEvent", function(self)
 		amnibdb.accept = true
 	end
 	
-	if amnibdb.instance then 
-		currentInstance = amnibdb.instance
-	end
-
-	acceptInvites = amnibdb.accept
 	local invites = "enabled"
 	if not amnibdb.accept then
 		invites = "disabled"
 	end
 	local instanceText = ""
-	if currentInstance then 
-		instanceText = string.format(" for %s", currentInstance)
+	if amnibdb.instance then 
+		instanceText = string.format(" for %s", amnibdb.instance)
 	end
-	Print("", "", string.format("Auto-invites are currently %s%s.", invites, instanceText))
+	Print(string.format("Auto-invites are currently %s%s.", invites, instanceText))
+
+	if not amnibdb.text then
+		amnibdb.text = defaultText
+	end
 end)
 
-local version = "1.2.2"
+local version = "1.3"
 
 local inviter = CreateFrame"Frame"
 inviter:RegisterEvent"CHAT_MSG_WHISPER"
 inviter:SetScript("OnEvent", function(self, event, what, who)
-	if what == "invite" and acceptInvites then
+	if amnibdb.accept and inviteKeywords[what] then
 		InviteUnit(who)
 	end
 end)
 
-
-
-local function showHelp(invites)
-	Print("", "", "/ib, /amnib")
-	Print("- ", "/ib start <instance>",  "Starts the auto-invites and broadcasts to the guild if instance specified.")
-	Print("- ", "/ib stop", "Stops the auto-invites.")
-	Print("- ", "/ib announce", "Announces the previously started instance invite.")
-	Print("- ", "/ib version", "Shows you what version you are using.")
-end
-
 local function toggleAcceptInvites(value)
-	if value == acceptInvites then return end
-	acceptInvites = value
+	if value == amnibdb.accept then return end
 	amnibdb.accept = value
-	if value then 
-		Print("", "", "Auto-invites enabled, type /ib stop to disable.")
+	if value then
+		Print("Auto-invites enabled, type /ib stop to disable.")
 	else
-		Print("", "", "Auto-invites disabled, type /ib start to enable.")
+		Print("Auto-invites disabled, type /ib start <instance> to enable.")
 	end
 end
 
-local function broadcastInvite(instance)
-	if not instance then return end
-
-	SendChatMessage(string.format("Invites for %s have started. For an invite, please whisper me 'invite' without the quotes.", instance), "GUILD")
+local function broadcastInvite()
+	SendChatMessage(string.format(amnibdb.text, amnibdb.instance), "GUILD")
 	toggleAcceptInvites(true)
 end
 
-SlashCmdList['INVITE_BROADCAST'] = function(arg1)
-	if string.sub(arg1, 0, 5) == "start" then 
-		local where = string.sub(arg1, 7)
-		if where ~= "" then 
-			broadcastInvite(where)
-			currentInstance = where
-			amnibdb.instance = where
+local function massInvite(units) 
+	for u in units do
+		InviteUnit(u)	
+	end
+end
+
+SlashCmdList['INVITE_BROADCAST'] = function(args)
+	local cmd, params = string.match(args, '^(%a+)%s*(.*)')
+	if cmd == 'start' then 
+		if params and params ~= "" then 
+			broadcastInvite(params)
+			amnibdb.instance = params
 		end
 		toggleAcceptInvites(true)
-	elseif arg1 == "stop" then
-		toggleAcceptInvites(false)
-		currentInstance = nil
+	elseif cmd == 'stop' then
 		amnibdb.instance = nil
-	elseif arg1 == "announce" then 
-		if currentInstance and acceptInvites then 
-			broadcastInvite(currentInstance)
+		toggleAcceptInvites(false)
+	elseif cmd == 'announce' then 
+		if amnibdb.instance and amnibdb.accept then 
+			broadcastInvite(amnibdb.instance)
 		else
-			Print("", "", "You have no instance invites started. To start an invite, please use /ib start <instance>.")
+			Print("You have no instance invites started. To start an invite, please use /ib start <instance>.")
 		end
-	elseif arg1 == "version" then 
-		Print("", "", "Amnith's Invite Broadcaster version: "..version)
+	elseif cmd == 'text' then
+		if params and params ~= "" then
+			amnibdb.text = params
+		end
+		GreenPrint("Current invite message", string.format(amnibdb.text, amnibdb.instance))
+	elseif cmd == 'resettext' then
+		amnibdb.text = defaultText
+		GreenPrint("Current invite message", string.format(amnibdb.text, amnibdb.instance))
+	elseif cmd == "version" then 
+		Print("Amnith's Invite Broadcaster version: "..version)
+	elseif cmd == 'massinv' then
+		local units = string.gmatch(params, '(%a+)')
+		massInvite(units)
 	else
-		showHelp()
+		GreenPrint("Amnith's Invite Broadcaster", "/ib, /amnib")
+		GreenPrint("/ib start <instance>", "Starts the auto-invites and broadcasts to the guild if instance specified.")
+		GreenPrint("/ib stop", "Stops the auto-invites.")
+		GreenPrint("/ib announce", "Announces the previously started instance invite.")
+		GreenPrint("/ib version", "Shows you what version you are using.")
+		GreenPrint("/ib text", "Shows current text to broadcast.")
+		GreenPrint("/ib text <text>", "Allows to set the text for invites. %s specifies the instance.")
+		GreenPrint("/ib resettext", "Resets invite text to default.")
 	end
 end
 SLASH_INVITE_BROADCAST1 = '/ib'
