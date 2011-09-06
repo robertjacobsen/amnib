2b645a4d6a5f5ec26b9ad6980f75e1037c97f8fe
Robert Jacobsen <robert@robertjacobsen.net>
2011-09-06 20:59:05 +0200

    Added more functionality. Let the version follow the game major version.

--- amnib.lua
+++ amnib.lua
@@ -1,11 +1,12 @@
 amnibdb = {}
+
+local version = "4.2"
+local defaultText = "Invites for <INSTANCE> have started. For an invite, please whisper me '<KEYWORD>' without the quotes.";
 local inviteKeywords = {
 	["inv"] = true,
 	["invite"] = true,
-	["inviter"] = true,
 	["'invite' without the quotes"] = true
 }
-local defaultText = "Invites for %s have started. For an invite, please whisper me 'invite' without the quotes.";
 
 local function GreenPrint(green, text)
 	if not text or not green then return end
@@ -17,14 +18,12 @@ local function Print(text)
 	GreenPrint("amnib", text)
 end
 
--- [[ Initialization of the SavedVariable ]]
-local addon = CreateFrame"Frame"
-addon:RegisterEvent"PLAYER_LOGIN"
-addon:SetScript("OnEvent", function(self)
-	if amnibdb.accept == nil then
-		amnibdb.accept = true
-	end
-	
+local function Error(text)
+	if not text then return end
+	ChatFrame1:AddMessage(string.format("|cffff0000amnib|r: %s", text))
+end
+
+local function Status()
 	local invites = "enabled"
 	if not amnibdb.accept then
 		invites = "disabled"
@@ -34,18 +33,35 @@ addon:SetScript("OnEvent", function(self)
 		instanceText = string.format(" for %s", amnibdb.instance)
 	end
 	Print(string.format("Auto-invites are currently %s%s.", invites, instanceText))
+end
 
+local addon = CreateFrame"Frame"
+addon:RegisterEvent"PLAYER_LOGIN"
+addon:SetScript("OnEvent", function(self)
+	if amnibdb.accept == nil then
+		amnibdb.accept = false 
+	end
+	
 	if not amnibdb.text then
 		amnibdb.text = defaultText
 	end
+
+	if not amnibdb.keywords then
+		amnibdb.keywords = inviteKeywords
+	end
+
+	if not amnibdb.default then
+		amnibdb.default = "invite"
+	end
+
+	Status()
 end)
 
-local version = "1.3"
 
 local inviter = CreateFrame"Frame"
 inviter:RegisterEvent"CHAT_MSG_WHISPER"
 inviter:SetScript("OnEvent", function(self, event, what, who)
-	if amnibdb.accept and inviteKeywords[what] then
+	if amnibdb.accept and amnibdb.keywords[what] then
 		InviteUnit(who)
 	end
 end)
@@ -53,15 +69,11 @@ end)
 local function toggleAcceptInvites(value)
 	if value == amnibdb.accept then return end
 	amnibdb.accept = value
-	if value then
-		Print("Auto-invites enabled, type /ib stop to disable.")
-	else
-		Print("Auto-invites disabled, type /ib start <instance> to enable.")
-	end
+	Status()
 end
 
 local function broadcastInvite()
-	SendChatMessage(string.format(amnibdb.text, amnibdb.instance), "GUILD")
+	SendChatMessage(amnibdb.text:gsub('\<INSTANCE\>', amnibdb.instance):gsub('\<KEYWORD\>', amnibdb.default), "GUILD")
 	toggleAcceptInvites(true)
 end
 
@@ -72,44 +84,79 @@ local function massInvite(units)
 end
 
 SlashCmdList['INVITE_BROADCAST'] = function(args)
-	local cmd, params = string.match(args, '^(%a+)%s*(.*)')
+	local cmd, params = args:match'^(%a+)%s*(.*)'
 	if cmd == 'start' then 
 		if params and params ~= "" then 
-			broadcastInvite(params)
 			amnibdb.instance = params
+			toggleAcceptInvites(true)
 		end
-		toggleAcceptInvites(true)
+		if amnibdb.instance then
+			broadcastInvite()
+		else
+			Error"Need an instance specified to broadcast invites."
+		end
 	elseif cmd == 'stop' then
 		amnibdb.instance = nil
 		toggleAcceptInvites(false)
 	elseif cmd == 'announce' then 
 		if amnibdb.instance and amnibdb.accept then 
-			broadcastInvite(amnibdb.instance)
+			broadcastInvite()
 		else
-			Print("You have no instance invites started. To start an invite, please use /ib start <instance>.")
+			Error"You have no instance invites started. To start an invite, please use /ib start <instance>."
 		end
 	elseif cmd == 'text' then
 		if params and params ~= "" then
 			amnibdb.text = params
 		end
-		GreenPrint("Current invite message", string.format(amnibdb.text, amnibdb.instance))
+		GreenPrint("Current invite message", amnibdb.text:format(amnibdb.instance))
 	elseif cmd == 'resettext' then
 		amnibdb.text = defaultText
-		GreenPrint("Current invite message", string.format(amnibdb.text, amnibdb.instance))
+		GreenPrint("Current invite message", amnibdb.text:format(amnibdb.instance))
 	elseif cmd == "version" then 
-		Print("Amnith's Invite Broadcaster version: "..version)
-	elseif cmd == 'massinv' then
-		local units = string.gmatch(params, '(%a+)')
-		massInvite(units)
+		Print(string.format("Amnith's Invite Broadcaster version: %s", version))
+	elseif cmd == 'massinv' or cmd == 'mi' then
+		massInvite(params:gmatch'(%a+)')
+	elseif cmd == 'keyword' then
+		local subcmd, word = params:match'(%a+)%s*(.*)'
+		if subcmd == 'add' and word and word ~= "" then 
+			amnibdb.keywords[word] = true
+		elseif subcmd == 'del' and word and word ~= "" then 
+			if word == amnibdb.default then 
+				Error"You cannot remove the default invite keyword."
+			else
+				amnibdb.keywords[word] = nil
+			end
+		elseif subcmd == 'default' and word and word ~= "" then
+			if amnibdb.keywords[word] then 
+				amnibdb.default = word
+				Print(string.format("%s is now the default keyword", word))
+			else
+				Error(string.format("There is no such keyword '%s'.", word))
+			end
+		end
+
+		local keywords = {}
+		table.foreach(amnibdb.keywords, function(key, val) 
+			if key == amnibdb.default then
+				key = string.format("|cffff8000%s|r", key)
+			end
+			table.insert(keywords, key)
+		end)
+		GreenPrint("Current invite keywords", table.concat(keywords, ', '))
+	elseif cmd == 'status' then
+		Status()
 	else
 		GreenPrint("Amnith's Invite Broadcaster", "/ib, /amnib")
 		GreenPrint("/ib start <instance>", "Starts the auto-invites and broadcasts to the guild if instance specified.")
 		GreenPrint("/ib stop", "Stops the auto-invites.")
+		GreenPrint("/ib status", "Shows the current status of the invite broadcaster.")
 		GreenPrint("/ib announce", "Announces the previously started instance invite.")
-		GreenPrint("/ib version", "Shows you what version you are using.")
+		GreenPrint("/ib massinv <player1> [player2] [player3] [...]", "Performs a mass invite for all players passed.")
 		GreenPrint("/ib text", "Shows current text to broadcast.")
-		GreenPrint("/ib text <text>", "Allows to set the text for invites. %s specifies the instance.")
+		GreenPrint("/ib text <text>", "Allows to set the text for invites. <INSTANCE> specifies the instance, <KEYWORD> specifies the default invite keyword.")
 		GreenPrint("/ib resettext", "Resets invite text to default.")
+		GreenPrint("/ib keyword [<add|del|default> keyword]", "With no parameters, lists current auto-invite keywords. Otherwise adds or removes given keyword, or sets it to the standard for broadcasting.")
+		GreenPrint("/ib version", "Shows you what version you are using.")
 	end
 end
 SLASH_INVITE_BROADCAST1 = '/ib'
